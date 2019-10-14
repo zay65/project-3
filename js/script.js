356b77f2ad2de178e5ce1cb3e2e7cc079263c37b
zay <zvnblue@gmail.com>
2019-10-13 20:54:50 -0400

    hide

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
-console.log($('Name') );
-console.log($( "#clickme" ));
 
 //Select the "Name" input element
 $('#name').focus();
 
+//This will hide "other" input field until user clicks on the "Other" option
+
+ $('#title').change (function () {
+     if ($('#title').val() === 'other'){
+         $('#other-title').show();
+     } else {
+         $('#other-title').hide();
+     }
+ });
+ 
 
 
-//display "Other" input field during javascript disabled
 
-$('Your Job Role').hide();
+
+
+
+
+
+//Theme is selected and 
+$('colors-js-puns').on('click', function() {
+$('color').show();
+});
+
+$('option[value="Select Theme"]').attr("hidden", "hidden");
